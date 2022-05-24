40973fbbca073d5e10c78cee0c9dd467ca28c2c2
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-24 15:27:48 +0800

    Resubmit Assignment
    Fixed code by adding Console.Read(); before each break line in the  switch statement.

--- ConsoleAppAs/ConsoleAppAs/Program.cs
+++ ConsoleAppAs/ConsoleAppAs/Program.cs
@@ -11,61 +11,69 @@ namespace ConsoleAppAs
         static void Main(string[] args)
         {
             // This is the while statement
-            //Console.WriteLine("Let's play a game, guess my favorite color.");
-            //string color = Console.ReadLine();
-            //bool rightColor = color == "green";
-            //while(!rightColor)
-            //{
-            //    switch(color)
-            //    {
-            //        case "blue":
-            //            Console.WriteLine("Nope blue is not the one.");
-            //            break;
-            //        case "red":
-            //            Console.WriteLine("Nah red is not my favorite color");
-            //            break;
-            //        case "yellow":
-            //            Console.WriteLine("Nope yellow is my second favorite color sorry.");
-            //            break;
-            //        case "green":
-            //            Console.WriteLine("Yup that's the one!");
-            //            break;
-            //        default:
-            //            Console.WriteLine("Keep Guessing");
-            //            break;
-            //}
-            // End of while statement
-
-            // This is the do while statement
-            Console.WriteLine("Let's play a game, guess my favorite color.\n");
             bool rightColor = false;
-            do
-                {
-                Console.WriteLine("What is it?");
+            
+            
+                Console.WriteLine("Let's play a game, guess my favorite color.");
                 string color = Console.ReadLine();
-                switch (color)
-                    {
-                        case "blue":
-                            Console.WriteLine("Nope blue is not the one.\n");
-                            break;
-                        case "red":
-                            Console.WriteLine("Nah red is not my favorite color\n");
-                            break;
-                        case "yellow":
-                            Console.WriteLine("Nope yellow is my second favorite color sorry.\n");
-                            break;
-                        case "green":
-                            Console.WriteLine("Yup that's the one!");
-                            rightColor = true;
-                            break;
-                        default:
-                            Console.WriteLine("Keep Guessing\n");
-                            break;
-                    }
-                } 
-                while (!rightColor);
-                Console.ReadLine();
-            }
+                    while (!rightColor)
+                    switch (color)
+                            {
+                                case "blue":
+                                    Console.WriteLine("Nope blue is not the one.");
+                                    color = Console.ReadLine();
+                                    break;
+                                case "red":
+                                    Console.WriteLine("Nah red is not my favorite color");
+                                        color = Console.ReadLine();
+                                    break;
+                                case "yellow":
+                                    Console.WriteLine("Nope yellow is my second favorite color sorry.");
+                                        color = Console.ReadLine();
+                                    break;
+                                case "green":
+                                    Console.WriteLine("Yup that's the one!");
+                                        color = Console.ReadLine();
+                                    rightColor = true;
+                                    break;
+                                default:
+                                    Console.WriteLine("Keep Guessing");
+                                        color = Console.ReadLine();
+                                    break;
+                            }
+                // End of while statement
+
+                // This is the do while statement
+                //Console.WriteLine("Let's play a game, guess my favorite color.\n");
+                //bool rightColor = false;
+                //do
+                //    {
+                //    Console.WriteLine("What is it?");
+                //    string color = Console.ReadLine();
+                //    switch (color)
+                //        {
+                //            case "blue":
+                //                Console.WriteLine("Nope blue is not the one.\n");
+                //                break;
+                //            case "red":
+                //                Console.WriteLine("Nah red is not my favorite color\n");
+                //                break;
+                //            case "yellow":
+                //                Console.WriteLine("Nope yellow is my second favorite color sorry.\n");
+                //                break;
+                //            case "green":
+                //                Console.WriteLine("Yup that's the one!");
+                //                rightColor = true;
+                //                break;
+                //            default:
+                //                Console.WriteLine("Keep Guessing\n");
+                //                break;
+                //        }
+                //    } 
+                //    while (!rightColor);
+                //    Console.ReadLine();
+            
         }
     }
+}
 // End of do while statement
