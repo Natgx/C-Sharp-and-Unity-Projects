5894471cb5636b011213aa787f0e6e811ad27e76
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-25 14:54:18 +0800

    Resubmitted Assignment
    Fixed code inside to not continue once requirements are not met for package weight and size and took off redundant code.

--- BranchingAs/BranchingAs/Program.cs
+++ BranchingAs/BranchingAs/Program.cs
@@ -13,24 +13,29 @@ namespace BranchingAs
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("Please enter the package weight:");
             int pckweight = Convert.ToInt32(Console.ReadLine());
-            if(pckweight > 50)
+            if (pckweight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-            Console.WriteLine("Please enter the package width:");
-            int pckwidth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the package height:");
-            int pckheight = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Please enter the package length:");
-            int pcklength = Convert.ToInt32(Console.ReadLine());
-            int total = pckwidth + pckheight + pcklength;
-            if(total > 50)
+            else
             {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.WriteLine("Please enter the package width:");
+                int pckwidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                int pckheight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                int pcklength = Convert.ToInt32(Console.ReadLine());
+                int total = pckwidth + pckheight + pcklength;
+                if (total > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    decimal combined = Convert.ToDecimal(((pckheight * pckwidth * pcklength) * pckweight) / 100.00);
+                    Console.WriteLine("Your estimated total for shipping this package is: " + "$" + combined + " \nThank you!");                    
+                }
             }
-            decimal combined = Convert.ToDecimal(((pckheight * pckwidth * pcklength) * pckweight) / 100.00);
-            decimal estTotal = Convert.ToDecimal(combined);
-            Console.WriteLine("Your estimated total for shipping this package is: " + "$" + estTotal + " \nThank you!");
             Console.ReadLine();
         }
     }
