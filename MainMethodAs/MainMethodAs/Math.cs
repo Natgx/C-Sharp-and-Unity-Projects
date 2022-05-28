275e06783d61439b6c2a96df1af276dfb4918e1f
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-28 17:11:51 +0800

    MAIN METHOD SUBMISSION ASSIGNMENT
    Submitted Code, hopefully meeting the requirements of the assignment.

---
+++ MainMethodAs/MainMethodAs/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAs
+{
+    public class Math
+    {
+        public static int Multiplication(int num1)
+        {
+            int newNum = num1 * 50;
+            return newNum;
+        }
+        public static decimal Division(decimal num2)
+        {
+            decimal newNUm = num2 / 5;
+            return newNUm;
+        }
+        public static int Addition(string num3)
+        {
+            int newNum = Convert.ToInt32(num3);
+            int newNum2 = newNum + 5;
+            return newNum2;
+        }
+    }
+}
---
+++ MainMethodAs/MainMethodAs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math operation = new Math();
+            Console.WriteLine("Give me a number to do a math operation on.");
+            int numChosen = Convert.ToInt32(Console.ReadLine());
+            int totalMul = Math.Multiplication(numChosen);
+            Console.WriteLine("Here is the result after multiplyinh your given number with fifty: " + totalMul + "\n");
+            Math divOp = new Math();
+            Console.WriteLine("Now give me another number to do another operation on.");
+            decimal numChosen2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Here is the result after dividing your given number by five: " + Math.Division(numChosen2) + "\n");
+            Math addOp = new Math();
+            Console.WriteLine("Now give me one number or text to do one last opertion on.");
+            string userString = Console.ReadLine();
+            int totalAdd = Math.Addition(userString);
+            // PLEASE LOOK AT THIS
+            // int totalAdd = addOp.Addition(userString); Why doesn't this work? Even though I have instantiated it.
+            Console.WriteLine("Here is the result when adding your number/text with the number five: " + totalAdd);
+            Console.ReadLine();
+        }
+    }
+}
