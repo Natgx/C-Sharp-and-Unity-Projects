6c2f22b6cec87cf91402bb760147f95b338b79a3
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-28 15:38:16 +0800

    CALLING METHODS SUBMISSION ASSIGNMENT
    Submitted Code satisfying all requirements of the assignment.

---
+++ CallingMethodsAs/CallingMethodsAs/Math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAs
+{
+    public class Math
+    {
+        public static int Addition(int num1)
+        {
+            int newNum = num1 + 10;
+            return newNum;
+        }
+        public static int Subtraction(int num2)
+        {
+            int newNum = num2 - 10;
+            return newNum;
+        }
+        public static int Multiplication(int num3)
+        {
+            int newNum = num3 * 10;
+            return newNum;
+        }
+    }
+}
---
+++ CallingMethodsAs/CallingMethodsAs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, give me a number to do a math operation on.");
+            int numChosen = Convert.ToInt32(Console.ReadLine());
+            int totalAdd = Math.Addition(numChosen);
+            int totalSub = Math.Subtraction(numChosen);
+            int totalMul = Math.Multiplication(numChosen);
+            Console.WriteLine("Here is the solution when we added the number you gave with 10.");
+            Console.WriteLine(totalAdd + "\n");
+            Console.WriteLine("Here is the solution when we subtracted the number you gave with 10.");
+            Console.WriteLine(totalSub + "\n");
+            Console.WriteLine("Here is the solution when we multiplied the number you gave with 10");
+            Console.WriteLine(totalMul);
+            Console.ReadLine();
+        }
+    }
+}
