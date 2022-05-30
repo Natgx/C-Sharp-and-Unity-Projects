058f57c481994fe720a51702e8477c4d19d7064d
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-30 18:25:49 +0800

    METHOD CLASS SUBMISSION ASSIGNMENT
    Submitted Assignment, satisfying its requirements.

---
+++ MethodClsAs/MethodClsAs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClsAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidMeth result = new voidMeth();
+            Console.WriteLine("Please give me two numbers.\n");
+            Console.WriteLine("What is the first number you would want to give me: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now what is the second number you would like to give me: ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            result.twoNums(num1: firstNum, num2: secondNum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClsAs/MethodClsAs/voidMeth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClsAs
+{
+    public class voidMeth
+    {
+        public void twoNums(int num1, int num2)
+        {
+            int newNum = num1 - 1;
+            Console.WriteLine("Here is the first number you gave me subtracted by one: " + newNum + "\nThis is the second number you gave me that I did nothing with: " + num2);
+        }
+    }
+}
