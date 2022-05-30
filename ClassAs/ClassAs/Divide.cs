80ab2ea37c67bb3941a75a5ab8843543475e7f9c
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-30 17:18:54 +0800

    CLASS SUBMISSION ASSIGNMENT
    Submitted Code, satisfying the requirements of the assignment.

---
+++ ClassAs/ClassAs/Divide.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAs
+{
+    public class Divide
+    {
+        public void Division(int num1)
+        {
+
+
+            int newNum = num1 / 2;
+            Console.WriteLine(newNum);
+        }
+
+        public void Division(int num2, out int timesDivided, int times = 1)
+        {
+            timesDivided = 0;
+            for(int i = 0; i < times; i++)
+            {
+                timesDivided++;
+                int newNum = num2 / 2;
+                Console.WriteLine(newNum);
+            }
+
+        }
+    }
+}
---
+++ ClassAs/ClassAs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divide varDiv = new Divide();
+            Console.WriteLine("Hello give me a whole number to divide it by 2.");
+            int givenNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Here is the result after dividing your given number by 2:");
+            varDiv.Division(givenNumber);
+
+            int timesDivided = 0;
+            Console.WriteLine("Now give me 2 numbers, one will be the number we're dividing and the other one will be how many times we're dividing it by 2.");
+            int twoNumbers1 = Convert.ToInt32(Console.ReadLine());
+            int twoNumbers2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Here is the result: ");
+            varDiv.Division(twoNumbers1, out timesDivided, twoNumbers2);
+            Console.WriteLine("As a reminder, this is how many times you wanted us to divide your given number: " + timesDivided);
+            Console.ReadLine();
+        }
+    }
+}
