40c68335c6aa0e08121e7f8fa204b9bcd8756f35
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-30 18:04:59 +0800

    METHOD SUBMISSION ASSIGNMENT
    Submitted assignment

---
+++ MethodAs/MethodAs/ParamTry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAs
+{
+    public class ParamTry
+    {
+        public static int Addition(int num1, int num2 = 1)
+        {
+            int newNum = num1 + num2;
+            return newNum;
+        }
+    }
+}
---
+++ MethodAs/MethodAs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamTry addMeth = new ParamTry();
+            Console.WriteLine("Please give me two numbers to do a addition operation on, note that it is not required to give me the second number. \n");
+            Console.WriteLine("What is the first number: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now what is the second number:");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nHere is the result when we add the number\\s you gave me:\n" + ParamTry.Addition(firstNum, secondNum));
+            Console.ReadLine();
+        }
+    }
+}
