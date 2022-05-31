b9b117449a8723cce9f8a5b5b7771d08650266ca
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-31 20:42:49 +0800

    ABSTRACT CLASS SUBMISSION ASSIGNMENT
    Submitted code, satisfying the requirements of the assignment.

---
+++ AbstractClsAs/AbstractClsAs/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClsAs
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Here is the full name of the employee:");
+            base.SayName();
+        }
+        
+
+        
+    }
+}
