78fe34b0258bb13679c1c66c6db10197c154374b
Natgx <102414039+Natgx@users.noreply.github.com>
2022-05-31 18:37:26 +0800

    METHOD SUBMISSION ASSIGNMENT
    Resubmitting assignment, hopefully satisfying its requirements. Added try and catch blocks to make use of optional parameters.

--- MethodAs/MethodAs/Program.cs
+++ MethodAs/MethodAs/Program.cs
@@ -10,13 +10,22 @@ namespace MethodAs
     {
         static void Main(string[] args)
         {
-            ParamTry addMeth = new ParamTry();
-            Console.WriteLine("Please give me two numbers to do a addition operation on, note that it is not required to give me the second number. \n");
-            Console.WriteLine("What is the first number: ");
-            int firstNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Now what is the second number:");
-            int secondNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nHere is the result when we add the number\\s you gave me:\n" + ParamTry.Addition(firstNum, secondNum));
+            
+            
+                ParamTry addMeth = new ParamTry();
+                Console.WriteLine("Please give me two numbers to do a addition operation on, note that it is not required to give me the second number. \n");
+                Console.WriteLine("What is the first number: ");
+                int firstNum = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Now what is the second number:");
+                int secondNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nHere is the result when we add the number\\s you gave me:\n" + ParamTry.Addition(firstNum, secondNum));
+            }
+            catch
+            {
+                Console.WriteLine("\nHere is the result when we add the number\\s you gave me:\n" + ParamTry.Addition(firstNum, 1));
+            }
             Console.ReadLine();
         }
     }
