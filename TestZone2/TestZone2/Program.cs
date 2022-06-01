85c3674d0d0cb30e65324bf3b699e73433323ced
Natgx <102414039+Natgx@users.noreply.github.com>
2022-06-01 18:26:18 +0800

    PARAMETERS SUBMISSION ASSIGNMENT
    Submitted Code satisfying the requirements of the assignment.

---
+++ TestZone2/TestZone2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestZone2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> stringList = new Employee<string>();
+            stringList.things = new List<string>() { "These", "are", "strings" };
+            Employee<int> intList = new Employee<int>();
+            intList.things = new List<int>() { 1, 2, 3 };
+            foreach (string word in stringList.things)
+            {
+                Console.WriteLine(word);
+            }
+            foreach (int number in intList.things)
+            {
+                Console.WriteLine(number);
+            }
+            // End of Generic Parameters Assignment
+            Console.ReadLine();
+
+        }
+    }
+}
