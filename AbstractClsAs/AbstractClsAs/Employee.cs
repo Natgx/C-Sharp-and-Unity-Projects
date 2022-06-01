9a1efa226c2561ef71b417e4efacdc5a8eb0a41a
Natgx <102414039+Natgx@users.noreply.github.com>
2022-06-01 17:13:58 +0800

    OPERATORS SUBMISSION ASSIGNMENT
    Submitted Code satisfying all the requirements of the assignment.

--- AbstractClsAs/AbstractClsAs/Employee.cs
+++ AbstractClsAs/AbstractClsAs/Employee.cs
@@ -8,9 +8,12 @@ namespace AbstractClsAs
 {
     public class Employee : Person, IQuittable
     {
+        public int Id { get; set; }
+        
+        
         public override void SayName()
         {
-            Console.WriteLine("Here is the full name of one of the employee:");
+            Console.WriteLine("Here is the full name of one of the employees:");
             Console.WriteLine(firstName + lastName);
         }
         
@@ -18,6 +21,15 @@ namespace AbstractClsAs
         {
             Console.WriteLine(firstName + lastName + " quitted yesterday.");
         }
-        
+        public static bool operator== (Employee Id, Employee Id2)
+        {
+            bool truth = Id == Id2;
+            return truth;
+        }
+        public static bool operator!= (Employee Id, Employee Id2)
+        {
+            bool whut = Id != Id2;
+            return whut;
+        }
     }
 }
--- AbstractClsAs/AbstractClsAs/Program.cs
+++ AbstractClsAs/AbstractClsAs/Program.cs
@@ -10,14 +10,21 @@ namespace AbstractClsAs
     {
         static void Main(string[] args)
         {
-            Employee person = new Employee() { firstName = "Sample ", lastName = "Student" };
+            Employee person = new Employee() { firstName = "Sample ", lastName = "Student", Id = 4 };
             person.SayName();
 
             // Start of Polymorphism assignment
-            Console.WriteLine("\nHere is the name of one employee that quitted yesterday: ");
-            IQuittable quit = new Employee() { firstName = "I ", lastName = "Quit" };
-            quit.Quit();
+            //Console.WriteLine("\nHere is the name of one employee that quitted yesterday: ");
+            //IQuittable quit = new Employee() { firstName = "I ", lastName = "Quit", Id = 5 };
+            //quit.Quit();
             // End of Polymorphism assignment
+            // Overloading operators assignment
+            Employee person2 = new Employee() { firstName = "John ", lastName = "Cena", Id = 10 };
+            person2.SayName();
+            Console.WriteLine("\nDo the ID's of these two employees stated above match?");
+            bool truth = person.Id == person2.Id;
+            Console.WriteLine(truth);
+            // End of Overloading operators assignment
             Console.ReadLine();
         }
     }
